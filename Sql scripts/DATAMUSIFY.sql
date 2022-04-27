5fac2f5ab804e3d32b29ca0df32814e8b14d0a20
Prod23 <88609959+Prod23@users.noreply.github.com>
2022-04-27 23:46:52 +0530

    Add files via upload

---
+++ Sql scripts/DATAMUSIFY.sql
@@ -0,0 +1,146 @@
+
+INSERT INTO APPUSER VALUES (1,'123*4','Rishiraj',19,'rishi@gmail.com','Normal','D 21 Lodha','123 Street','Mumbai','Maharastra');
+INSERT INTO APPUSER VALUES (2,'aA*19','Anshul',20,'a19@gmail.com','Premium','M25 Rose Valley','23 Street','Hyderabad','Telengana');
+INSERT INTO APPUSER VALUES (3,'1@3$','Vaibhav',21,'vaibhav7@gmail.com','contributor','B01 Akashdeep Apts','11 Street','Banglore','Karnataka');
+INSERT INTO APPUSER VALUES (4,'45663@','Tushar',22,'tushar2020@gmail.com','contributor','A102 Hari Om Tower','22 Street','Hyderabad','Telengana');
+INSERT INTO APPUSER VALUES (5,'4455','Abhinav',19,'123abh@gmail.com','premium','H34 Dudley House','33 Street','Mumbai','Maharastra');
+INSERT INTO APPUSER VALUES (6,'4@52','Darshan',20,'dar@gmail.com','normal','A23 Rukmini Tower','44 Street','Mumbai','Maharastra');
+INSERT INTO APPUSER VALUES (7,'tT*19','Siddharth',21,'blaze2@gmail.com','premium','C304 Elite Palace','55 Street','Banglore','Karnataka');
+INSERT INTO APPUSER VALUES (8,'aA@19','Vaishnav',20,'vaishnav@gmail.com','contributor','F111 Rudraksh','66 Street','Hyderabad','Telengana');
+INSERT INTO APPUSER VALUES (9,'rR123','Varun',19,'manu@gmail.com','normal','K83 Arya Palace','77 Street','Hyderabad','Telengana');
+INSERT INTO APPUSER VALUES (10,'qqaa11','Shashank',20,'shanks@gmail.com','contributor','D311 Sky Palace','88 Street','Mumbai','Maharastra');
+
+INSERT INTO playlist VALUES (1001,'Downloads');
+INSERT INTO playlist VALUES (1007,'Uploaded');
+INSERT INTO playlist VALUES (1015,'Mobile');
+INSERT INTO playlist VALUES (1036,'SleepTime');
+INSERT INTO playlist VALUES (1087,'Favourites');
+INSERT INTO playlist VALUES (1011,'SiddharthsFav');
+INSERT INTO playlist VALUES (1111,'Party');
+INSERT INTO playlist VALUES (1102,'VarunsFav');
+INSERT INTO playlist VALUES (1055,'Morning');
+INSERT INTO playlist VALUES (1028,'Friends');
+
+INSERT INTO album VALUES (501,'Arijit Singh', 2);
+INSERT INTO album VALUES (505,'Arko', 2);
+INSERT INTO album VALUES (567,'Bareiley Ki Barfi', 2);
+INSERT INTO album VALUES (581,'LoveAajKal', 2);
+INSERT INTO album VALUES (546,'MSD', 2);
+INSERT INTO album VALUES (502,'ArmaanMalik', 5);
+INSERT INTO album VALUES (520,'KK', 5);
+INSERT INTO album VALUES (511,'Chichore', 5);
+INSERT INTO album VALUES (539,'KapoorAndSons', 5);
+INSERT INTO album VALUES (507,'Pritam', 7);
+INSERT INTO album VALUES (509,'RadheShyam', 7);
+
+INSERT INTO songs VALUES(1,'Bairaagi','"C:\Users\rishi\Dropbox\PC\Downloads\Bareilly-Ki-Barfi.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.58.04 PM.mpeg"');
+INSERT INTO songs VALUES(30,'Jaan Hai Meri','"C:\Users\rishi\Dropbox\PC\Downloads\radhe shyam.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 11.00.01 PM (1).mpeg"');
+INSERT INTO songs VALUES(92,'RABBA','"C:\Users\rishi\Dropbox\PC\Downloads\heropanti.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.58.10 PM.mpeg"');
+INSERT INTO songs VALUES(2,'Khariyat','"C:\Users\rishi\Dropbox\PC\Downloads\chichore.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 11.00.01 PM.mpeg"');
+INSERT INTO songs VALUES(4,'Aur Tanha','"C:\Users\rishi\Dropbox\PC\Downloads\loveaajkal.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.58.08 PM.mpeg"');
+INSERT INTO songs VALUES(107,'Saathi Rey','"C:\Users\rishi\Dropbox\PC\Downloads\kapoorand sons.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.58.02 PM.mpeg"');
+INSERT INTO songs VALUES(58,'Nazm Nazm','"C:\Users\rishi\Dropbox\PC\Downloads\Bareilly-Ki-Barfi.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.57.59 PM.mpeg"');
+INSERT INTO songs VALUES(6,'Shayaad','"C:\Users\rishi\Dropbox\PC\Downloads\loveaajkal.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.57.54 PM.mpeg"');
+INSERT INTO songs VALUES(18,'Tabhah','"C:\Users\rishi\Dropbox\PC\Downloads\heropanti.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.57.49 PM.mpeg"');
+INSERT INTO songs VALUES(76,'Jab Tak','"C:\Users\rishi\Dropbox\PC\Downloads\msd.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-25 at 10.57.47 PM.mpeg"');
+INSERT INTO songs VALUES(10,'Kal ki hi baat hai','"C:\Users\rishi\Dropbox\PC\Downloads\chichore.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\04 - Kal Ki Hi Baat Hai - Downloadming.SE.mp3"');
+INSERT INTO songs VALUES(5,'Bolna','"C:\Users\rishi\Dropbox\PC\Downloads\kapoorand sons.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\Bolna Lyric Video - Kapoor & SonsSidharthAliaFawadArijitAseesTanishk.mp3"');
+INSERT INTO songs VALUES(23,'Yeh Dooriyan','"C:\Users\rishi\Dropbox\PC\Downloads\loveaajkal.jpg"','"C:\Users\rishi\Dropbox\PC\Downloads\WhatsApp Audio 2022-04-27 at 9.18.27 PM.mpeg"');
+
+INSERT INTO Contributes VALUES (505,8);
+INSERT INTO Contributes VALUES (567,8);
+INSERT INTO Contributes VALUES (581,3);
+INSERT INTO Contributes VALUES (546,3);
+INSERT INTO Contributes VALUES (502,4);
+INSERT INTO Contributes VALUES (520, 3);
+INSERT INTO Contributes VALUES (511, 10);
+INSERT INTO Contributes VALUES (539,10);
+INSERT INTO Contributes VALUES (507,4);
+INSERT INTO Contributes VALUES (509,10);
+
+INSERT INTO Friend VALUES (2,5);
+INSERT INTO Friend VALUES (2,7);
+INSERT INTO Friend VALUES (5,7);
+INSERT INTO Friend VALUES (7,5);
+INSERT INTO Friend VALUES (5,2);
+INSERT INTO Friend VALUES (7,2);
+
+INSERT INTO Listen VALUES (1, 1);
+INSERT INTO Listen VALUES (2, 2);
+INSERT INTO Listen VALUES (2, 92);
+INSERT INTO Listen VALUES (3, 10);
+INSERT INTO Listen VALUES (4, 58);
+INSERT INTO Listen VALUES (5, 76);
+INSERT INTO Listen VALUES (5, 107);
+INSERT INTO Listen VALUES (6, 30);
+INSERT INTO Listen VALUES (7, 4);
+INSERT INTO Listen VALUES (8, 6);
+INSERT INTO Listen VALUES (8, 18);
+INSERT INTO Listen VALUES (9, 5);
+INSERT INTO Listen VALUES (10, 23);
+
+
+INSERT INTO CreatedBy VALUES (1001,1);
+INSERT INTO CreatedBy VALUES (1007,3);
+INSERT INTO CreatedBy VALUES (1015,5);
+INSERT INTO CreatedBy VALUES (1036,7);
+INSERT INTO CreatedBy VALUES (1087,8);
+INSERT INTO CreatedBy VALUES (1011,2);
+INSERT INTO CreatedBy VALUES (1111,10);
+INSERT INTO CreatedBy VALUES (1102,9);
+INSERT INTO CreatedBy VALUES (1055,6);
+INSERT INTO CreatedBy VALUES (1028,4);
+INSERT INTO CreatedBy VALUES (1102,6);
+INSERT INTO CreatedBy VALUES (1055,4);
+INSERT INTO CreatedBy VALUES (1028,9);
+
+
+INSERT INTO PartOf VALUES (1, 501);
+INSERT INTO PartOf VALUES (2, 501);
+INSERT INTO PartOf VALUES (58, 505);
+INSERT INTO PartOf VALUES (10, 520);
+INSERT INTO PartOf VALUES (6, 501);
+INSERT INTO PartOf VALUES (5, 501);
+INSERT INTO PartOf VALUES (76, 502);
+INSERT INTO PartOf VALUES (30, 502);
+INSERT INTO PartOf VALUES (4, 520);
+
+INSERT INTO PartOf VALUES (23, 507);
+
+INSERT INTO PartOf VALUES (18, 505);
+INSERT INTO PartOf VALUES (1, 567);
+INSERT INTO PartOf VALUES (2, 511);
+INSERT INTO PartOf VALUES (58, 567);
+INSERT INTO PartOf VALUES (10, 511);
+INSERT INTO PartOf VALUES (6, 581);
+INSERT INTO PartOf VALUES (5, 539);
+INSERT INTO PartOf VALUES (76, 546);
+INSERT INTO PartOf VALUES (30, 509);
+INSERT INTO PartOf VALUES (4, 581);
+INSERT INTO PartOf VALUES (23, 581);
+
+INSERT INTO PartPlaylist VALUES (1, 1001);
+INSERT INTO PartPlaylist VALUES (2, 1007);
+INSERT INTO PartPlaylist VALUES (58, 1015);
+INSERT INTO PartPlaylist VALUES (10, 1036);
+INSERT INTO PartPlaylist VALUES (6, 1087);
+INSERT INTO PartPlaylist VALUES (5, 1011);
+INSERT INTO PartPlaylist VALUES (76, 1111);
+INSERT INTO PartPlaylist VALUES (30, 1102);
+INSERT INTO PartPlaylist VALUES (4, 1055);
+INSERT INTO PartPlaylist VALUES (4, 1028);
+INSERT INTO PartPlaylist VALUES (23, 1001);
+INSERT INTO PartPlaylist VALUES (23, 1015);
+INSERT INTO PartPlaylist VALUES (92, 1087);
+INSERT INTO PartPlaylist VALUES (18, 1011);
+INSERT INTO PartPlaylist VALUES (18, 1102);
+INSERT INTO PartPlaylist VALUES (1, 1028);
+INSERT INTO PartPlaylist VALUES (2, 1001);
+INSERT INTO PartPlaylist VALUES (58, 1007);
+INSERT INTO PartPlaylist VALUES (10, 1087);
+INSERT INTO PartPlaylist VALUES (6, 1036);
+INSERT INTO PartPlaylist VALUES (5, 1028);
+INSERT INTO PartPlaylist VALUES (6, 1011);
+INSERT INTO PartPlaylist VALUES (10, 1007);
+INSERT INTO PartPlaylist VALUES (4, 1036);
+
